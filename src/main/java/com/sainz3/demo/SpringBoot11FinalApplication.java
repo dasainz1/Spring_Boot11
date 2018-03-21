fb62b0090eced11b814ba0d5efe5ebdddff5a5e5
Daniela Sainz <danielasainz@Danielas-Air.fios-router.home>
2018-03-21 10:48:46 -0400

    Initial commit

---
+++ src/main/java/com/sainz3/demo/SpringBoot11FinalApplication.java
@@ -0,0 +1,12 @@
+package com.sainz3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot11FinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot11FinalApplication.class, args);
+	}
+}
